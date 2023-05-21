b4ad31efa7cdd3ea8ece8ffc716c682d30de72b9
Jao Renzo Miranda <mirandajaorenzo@gmail.com>
2023-05-21 16:30:58 +0800

    Added shorten link functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState, useMemo } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const inputRef = useRef();
+  const [links, setLinks] = useState([]);
+
+  const renderedList = useMemo(() => {
+    return links.map((item, index) => (
+      <li key={index} className="flex justify-between">
+        <p>{item.original}</p>
+        <p>{item.shorten}</p>
+      </li>
+    ));
+  }, [links]);
+
+  const apiUrl = "https://api.shrtco.de/v2/shorten?url=";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(apiUrl + inputRef.current.value);
+      const data = await response.json();
+      console.log(data);
+      setLinks((prevLinks) => {
+        return [
+          ...prevLinks,
+          {
+            original: inputRef.current.value,
+            shorten: data.result.full_short_link,
+          },
+        ];
+      });
+    } catch (error) {
+      // Handle any errors that occurred during the data fetch
+      console.error("Error fetching data:", error);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form action="" onSubmit={(e) => handleSubmit(e)}>
+        <input ref={inputRef} type="text" className="border bg-neutral-500" />
+        <button type="submit">Shorten</button>
+      </form>
+      <ul className="bg-slate-500 w-full ">{renderedList}</ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
